3f6792e75384dc1297c97068989f9ef0db5ac1d4
Susy Anne <36510291+SAnny98@users.noreply.github.com>
2018-07-31 20:59:55 -0300

    Update CalculadoraSalario.java

--- ProjetoMatrix/src/main/java/br/com/cariocadev/ProjetoMatrix/CalculadoraSalario.java
+++ ProjetoMatrix/src/main/java/br/com/cariocadev/ProjetoMatrix/CalculadoraSalario.java
@@ -1,108 +1,81 @@
+
 package br.com.cariocadev.ProjetoMatrix;
 
 import java.math.BigDecimal;
 
 public class CalculadoraSalario {
+ 
+  public boolean valida(BigDecimal nota){
+    return (nota != null && nota.compareTo(BigDecimal.TEN)<=0 && nota.compareTo(BigDecimal.ZERO)>0);
+  }
 
-   public BigDecimal getSalarioLiquido(BigDecimal salarioBruto, BigDecimal pentualImpostoINSS) {
+  public BigDecimal getSalarioLiquido(BigDecimal salarioBruto, BigDecimal pentualImpostoINSS) {
+    
+    if(valida(salarioBruto)){ 
+      
+      if(salarioBruto.compareTo(BigDecimal.valueOf(1693.72)<=0){
+      	return salarioBruto.subtract(salarioBruto.multiply(BigDecimal.valueOf(0.08))).setScale(2, BigDecimal.ROUND_HALF_UP);
+      }
 
-    if(salarioBruto != null){ 
-         
-        if(salarioBruto<=1.693,72){
-          	return (salarioBruto -(salarioBruto*8/100));
-	}
-
-	else if((salarioBruto >=R$ 1.693,73) && ( salarioBruto <= R$ 2.822,90){
-        	return (salarioBruto - (salarioBruto*9/100));
-	}
-
-        else if((salarioBruto >R$ 2.822,91){
-        	return (salarioBruto - (salarioBruto*11/100));
-	}
-	
+       else if(salarioBruto.compareTo(BigDecimal.valueOf(1693.72))>0 && salarioBruto.compareTo(BigDecimal.valueOf(2822.90)<=0){
+         return  salarioBruto.subtract(salarioBruto.multiply(BigDecimal.valueOf(0.09))).seScale(2, BigDecimal.ROUND_HALF_UP);
+       }
+    
+       else {
+         return salarioBruto.subtract(salarioBruto.multiply(BigDecimal.valueOf(0.11))).setScale(2, BigDecimal.ROUND_HALF_UP));
+      }
     }
-    else{
-          throw new IllegalArgumentException();
-    }
+       throw new IllegalArgumentException();
 }   
-   	
 
    public BigDecimal getValorINSS(BigDecimal salarioBruto) {
-		
-           if(salarioBruto != null){ 
-         
-        if(salarioBruto<=1.693,72){
-          	return (salarioBruto*8/100);
-	}
-
-	else if((salarioBruto >=R$ 1.693,73) && ( salarioBruto <= R$ 2.822,90){
-        	return (salarioBruto*9/100);
-	}
-
-        else if((salarioBruto >R$ 2.822,91){
-        	return (salarioBruto*11/100);
-	}
-	
-    }
-    else{
-          throw new IllegalArgumentException();
-    }
-} 
-
-
-
-   public BigDecimal getValorPlanoDeSaude(Integer idade) {
-    int op;
-
-    System.out.println("Pressione o número correspondente a idade.");
-    System.out.println(" MENU ");
-    System.out.println(" 1) De 0 a 9 anos");
-    System.out.println(" 2) De 10 a 19 anos");
-    System.out.println(" 3) De 20 a 29 anos");
-    System.out.println(" 4) De 30 a 39 anos");
-    System.out.println(" 5) De 40 a 49 anos");
-    System.out.println(" 6) De 50 a 59 anos");
-    System.out.println(" 7) Acima de 60 anos");
-    op = Console.readInt(" Opção:  ");
      
+       if(valida(salarioBruto)){ 
+      
+       if(salarioBruto.compareTo(BigDecimal.valueOf(1693.72))<=0){
+
+           return salarioBruto.multiply(BigDecimal.valueOf(0.08));
+       }
+	  
+       else if(salarioBruto.compareTo(BigDecimal.valueOf(1693.72)) >0 && salarioBruto.compareTo(BigDecimal.valueOf(2822.90))<=0){
+          return (salarioBruto.multiply(BigDecimal.valueOf(0.09)));
+       }
+    
+       else {
+         return (salarioBruto.multiply(BigDecimal.valueOf(0.11));
+       }
+    }
+       throw new IllegalArgumentException();
+}   
+
+
+  public BigDecimal getValorPlanoDeSaude(Integer idade) {
+          
+     if(idade!= null && idade>=0){
                                               
-    if((idade!= null) || (idade>0)){ 	
-     switch (opcao) {
-	
-	case 1:
-           return salarioBruto - 75,00;
-	   break;
-
-	case 2:
-           return salarioBruto-  112,50;
-	   break;
-        
-        case 3:
-            return salarioBruto - 168,75;  
-	    break;
-         
-        case 4:
-           return salarioBruto - 253,13;
-	   break;	
-        
-        case 5:
-           return salarioBruto - 379,69;
-	   break;
-
-        case 6:
-           return salarioBruto - 569,54
-	   break;
-
-	case 7:
-           return salarioBruto - 854,30;
-	   break;
-	
-        default:
-	    System.out.println("Opção Inválida!");
-	    break;
+     if(idade>=0 && idade<=9){
+	 return BigDecimal.valueOf(75).setScale(2, BigDecimal.ROUND_HALF_UP));
+     }
+     else if(idade >=10 && idade<=19){
+               
+	 return BigDecimal.valueOf(112.50).setScale(2, BigDecimal.ROUND_HALF_UP));
+     }
+     else if(idade>=20 && idade<=29){
+	 return BigDecimal.valueOf(168.75).setScale(2, BigDecimal.ROUND_HALF_UP));
+     }
+     else if(idade>=30 && idade<=39){
+	 return BigDecimal.valueOf(253.13).setScale(2, BigDecimal.ROUND_HALF_UP));
+     }    
+     else if(idade>=40 && idade<=49){
+	 return BigDecimal.valueOf(379.69).setScale(2, BigDecimal.ROUND_HALF_UP));
+     } 
+     else if(idade>=50 && idade<=59){
+	 return BigDecimal.valueOf(569.54).setScale(2, BigDecimal.ROUND_HALF_UP));
+     } 
+     else if(idade>=60){
+   return BigDecimal.valueOf(854.30).setScale(2, BigDecimal.ROUND_HALF_UP));
      }	
    }
    else{
           throw new IllegalArgumentException();
-}
-} 
+   }
