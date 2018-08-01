74923aa6eae2b1f55cd54ab63f125724db46592f
Susy Anne <36510291+SAnny98@users.noreply.github.com>
2018-07-31 21:01:12 -0300

    Update CalculadoraSalario.java

--- ProjetoMatrix/src/main/java/br/com/cariocadev/ProjetoMatrix/CalculadoraSalario.java
+++ ProjetoMatrix/src/main/java/br/com/cariocadev/ProjetoMatrix/CalculadoraSalario.java
@@ -51,31 +51,29 @@ public class CalculadoraSalario {
 
   public BigDecimal getValorPlanoDeSaude(Integer idade) {
           
-     if(idade!= null && idade>=0){
+   if(idade!= null && idade>=0){
                                               
-     if(idade>=0 && idade<=9){
+      if(idade>=0 && idade<=9){
 	 return BigDecimal.valueOf(75).setScale(2, BigDecimal.ROUND_HALF_UP));
-     }
-     else if(idade >=10 && idade<=19){
-               
-	 return BigDecimal.valueOf(112.50).setScale(2, BigDecimal.ROUND_HALF_UP));
-     }
-     else if(idade>=20 && idade<=29){
+      }
+      else if(idade >=10 && idade<=19){
+          return BigDecimal.valueOf(112.50).setScale(2, BigDecimal.ROUND_HALF_UP));
+      }
+      else if(idade>=20 && idade<=29){
 	 return BigDecimal.valueOf(168.75).setScale(2, BigDecimal.ROUND_HALF_UP));
-     }
-     else if(idade>=30 && idade<=39){
+      }
+      else if(idade>=30 && idade<=39){
 	 return BigDecimal.valueOf(253.13).setScale(2, BigDecimal.ROUND_HALF_UP));
-     }    
-     else if(idade>=40 && idade<=49){
+      }    
+      else if(idade>=40 && idade<=49){
 	 return BigDecimal.valueOf(379.69).setScale(2, BigDecimal.ROUND_HALF_UP));
-     } 
-     else if(idade>=50 && idade<=59){
+      } 
+      else if(idade>=50 && idade<=59){
 	 return BigDecimal.valueOf(569.54).setScale(2, BigDecimal.ROUND_HALF_UP));
-     } 
-     else if(idade>=60){
-   return BigDecimal.valueOf(854.30).setScale(2, BigDecimal.ROUND_HALF_UP));
-     }	
+      } 
+      else if(idade>=60){
+         return BigDecimal.valueOf(854.30).setScale(2, BigDecimal.ROUND_HALF_UP));
+      }	
    }
-   else{
           throw new IllegalArgumentException();
    }
